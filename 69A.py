38a4f9ac6b6f0176391184a80b293edf571d6580
Praveen230102 <53830601+Praveen230102@users.noreply.github.com>
2020-05-24 16:18:20 +0530

    Create 69A.py

---
+++ 69A.py
@@ -0,0 +1,13 @@
+n = int(input())
+a= 0
+b=0
+c=0
+for i in range(n):
+    x, y, z = map(int, input().split())
+    a += x
+    b += y
+    c += z
+if a == 0 and b == 0 and c == 0:
+    print("YES")
+else:
+    print("NO")
