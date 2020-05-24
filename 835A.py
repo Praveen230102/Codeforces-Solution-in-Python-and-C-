cf7ce8ac9d01cf513c5897b5cc9ac80c01705c57
Praveen230102 <53830601+Praveen230102@users.noreply.github.com>
2020-05-24 16:44:32 +0530

    Create 835A.py

---
+++ 835A.py
@@ -0,0 +1,7 @@
+s, v1, v2, t1, t2 = map(int, input().split())
+if t1 + s*v1 + t1 > t2 + s*v2 + t2:
+    print("Second")
+elif t1 + s*v1 + t1 < t2 + s*v2 + t2:
+    print("First")
+else:
+    print("Friendship")
