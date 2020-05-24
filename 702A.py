00ed083916f3d141303f32ce84ed1fc2443d8167
Praveen230102 <53830601+Praveen230102@users.noreply.github.com>
2020-05-24 20:06:40 +0530

    Create 702A.py

---
+++ 702A.py
@@ -0,0 +1,15 @@
+n = int(input())
+arr = list(map(int, input().split()))
+nums = []
+count = 1
+for i in range(0, n):
+    for j in range(i+1, n):
+        if arr[i] < arr[j]:
+            count += 1
+            break
+        else:
+            nums.append(count)
+            count = 1
+            break
+nums.append(count)
+print(max(nums))
