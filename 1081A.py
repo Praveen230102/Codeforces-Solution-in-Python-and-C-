aa89389b59a4beb31c6804d479d18b6d66456d95
Praveen230102 <53830601+Praveen230102@users.noreply.github.com>
2020-05-25 08:30:47 +0530

    Create 1081A.py

---
+++ 1081A.py
@@ -0,0 +1,10 @@
+# bsdk idhar kya dekhne ko aaya hai, khud kr!!!
+# from math import *
+# from itertools import *
+# import random
+n = int(input())
+v = n
+if n <= 2:
+    print(n)
+else:
+    print(1)
