6d4a6efa8bd5deee687c43ebd2f9e8d9037e9020
Praveen230102 <53830601+Praveen230102@users.noreply.github.com>
2020-05-25 07:52:56 +0530

    Create 1196A.py

---
+++ 1196A.py
@@ -0,0 +1,7 @@
+# bsdk idhar kya dekhne ko aaya hai, khud kr!!!
+# from math import *
+# from itertools import *
+q = int(input())
+for i in range(q):
+    arr = list(map(int, input().split()))
+    print(sum(arr)//2)
