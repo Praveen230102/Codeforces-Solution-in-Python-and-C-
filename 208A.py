87692f6ac259978a5c4507ca17398bd7c15f20ca
Praveen230102 <53830601+Praveen230102@users.noreply.github.com>
2020-05-25 08:02:11 +0530

    Create 208A.py

---
+++ 208A.py
@@ -0,0 +1,12 @@
+# bsdk idhar kya dekhne ko aaya hai, khud kr!!!
+# from math import *
+# Anuradha's email is :
+# Laxmichitfund@25dinmepaisadouble.com
+# from itertools import *
+# import random
+s = input()
+lst = s.split("WUB")
+x = lst.count("")
+for i in range(x):
+    lst.remove("")
+print(" ".join(lst))
