6da67b8a2e8677e543a952b15da136c3dcd34d31
Praveen230102 <53830601+Praveen230102@users.noreply.github.com>
2020-05-25 07:49:32 +0530

    Create 4C.cpp

---
+++ 4C.cpp
@@ -0,0 +1,28 @@
+// earlier python code exceeds time limit so this code will run
+// thanks for watching you stalker
+// now go ad fuck off
+
+#include <cstdio>
+#include <iostream>
+#include <map>
+
+int main(){
+    
+    int n; 
+	scanf("%d\n", &n);
+    std::map<std::string,long> database;
+    std::string name("");
+
+    while(n--){
+        getline(std::cin, name);
+        if(database.find(name) == database.end())
+		{
+		puts("OK");database[name] = 1;
+		}
+        else
+		{
+		std::cout << name << database[name] << std::endl; ++database[name];
+		}
+    }
+    return 0;
+}
