f6f4d14fc288c263a35c147c9dc145b1cb188d85
Praveen230102 <53830601+Praveen230102@users.noreply.github.com>
2020-05-25 08:32:42 +0530

    Create 1351A.py

---
+++ 1351A.py
@@ -0,0 +1,8 @@
+# bsdk idhar kya dekhne ko aaya hai, khud kr!!!
+# from math import *
+# from itertools import *
+# import random
+t = int(input())
+for i in range(t):
+    a, b = map(int, input().split())
+    print(a + b)
