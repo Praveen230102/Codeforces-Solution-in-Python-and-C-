a902a7095bc5fc76e22df24b916945af5903e462
Praveen230102 <53830601+Praveen230102@users.noreply.github.com>
2020-05-25 08:17:54 +0530

    Create 672A.py

---
+++ 672A.py
@@ -0,0 +1,9 @@
+# bsdk idhar kya dekhne ko aaya hai, khud kr!!!
+# from math import *
+# from itertools import *
+# import random
+num = ""
+n = int(input())
+for i in range(1, 10000):
+    num += str(i)
+print(num[n - 1])
